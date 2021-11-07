18cecaadbab00cb1f2633b08b4735a78a30f0c3b
Rickard Lundin <rickard@x14.se>
2021-11-07 22:15:03 +0100

    autocommit to kafka

--- spark232job-mod/src/main/java/dk/ignalina/lab/spark232/RddSparkStreamingKafka.java
+++ spark232job-mod/src/main/java/dk/ignalina/lab/spark232/RddSparkStreamingKafka.java
@@ -1,13 +1,5 @@
 package dk.ignalina.lab.spark232;
 
-import jdk.nashorn.internal.runtime.linker.JavaAdapterFactory;
-import org.apache.spark.SparkConf;
-import org.apache.spark.SparkContext;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.sql.RowFactory;
-import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
 import com.twitter.bijection.Injection;
 import com.twitter.bijection.avro.GenericAvroCodecs;
 import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
@@ -15,32 +7,36 @@ import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.ByteArrayDeserializer;
-
-import org.apache.spark.TaskContext;
-import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.types.StructType;
 import org.apache.spark.streaming.Duration;
-import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
-import org.apache.spark.streaming.kafka010.*;
-
+import org.apache.spark.streaming.kafka010.ConsumerStrategies;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RddSparkStreamingKafka {
 
-private static Injection<GenericRecord, byte[]> recordInjection;
-private static Schema schema;
-private static String avroSchema = null;
-private static StructType schemaStructured = null;
+    private static Injection<GenericRecord, byte[]> recordInjection;
+    private static Schema schema;
+    private static String avroSchema = null;
+    private static StructType schemaStructured = null;
 
-    static  {
+    static {
 
         try {
-            avroSchema = Utils.getLastestSchema("http://10.1.1.90:8081","table_X14-value");
+            avroSchema = Utils.getLastestSchema("http://10.1.1.90:8081", "table_X14-value");
         } catch (IOException e) {
             e.printStackTrace();
         } catch (RestClientException e) {
@@ -49,27 +45,23 @@ private static StructType schemaStructured = null;
 
         Schema.Parser parser = new Schema.Parser();
         schema = parser.parse(avroSchema);
-// TODO checkout https://stackoverflow.com/questions/40789153/how-to-convert-avro-schema-object-into-structtype-in-spark
         recordInjection = GenericAvroCodecs.toBinary(schema);
-
-
     }
 
     static Utils.Config config;
 
 
     public static void main(String... args) {
-        System.out.println("spark 2.3.2 RDD kakfa V0.10+ streaming to hive");
         config = Utils.parToConfig(args);
 
 
-      SparkConf conf = new SparkConf().setAppName("v202111061807  spark 2.3.2 streaming job").
-              setMaster("spark://10.1.1.190:6066").
+        SparkConf conf = new SparkConf().setAppName("v202111061807  spark 2.3.2 streaming job to hive").
+                setMaster("spark://10.1.1.190:6066").
                 set("spark.sql.warehouse.dir", "/apps/hive/warehouse").
-                set("spark.submit.deployMode" , "cluster").
-                set("spark.sql.catalogImplementation","hive").
-                set("hive.metastore.uris","thrift://10.1.1.190:9083").
-                set("spark.driver.supervise","true").
+                set("spark.submit.deployMode", "cluster").
+                set("spark.sql.catalogImplementation", "hive").
+                set("hive.metastore.uris", "thrift://10.1.1.190:9083").
+                set("spark.driver.supervise", "true").
                 set("spark.hadoop.fs.defaultFS", "hdfs://10.1.1.190:8020").
                 set("spark.hadoop.fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName()).
                 set("spark.hadoop.fs.hdfs.server", org.apache.hadoop.hdfs.server.namenode.NameNode.class.getName()).
@@ -81,48 +73,28 @@ private static StructType schemaStructured = null;
                 appName("park 2.3.2 streaming job").
                 config("spark.sql.warehouse.dir", "/apps/hive/warehouse").
                 master("spark://10.1.1.190:6066").
-                config("spark.submit.deployMode","cluster").
+                config("spark.submit.deployMode", "cluster").
                 enableHiveSupport().
                 getOrCreate();
 
-         spark.sql("use hiveorg_prod");
+        spark.sql("use hiveorg_prod");
 
 
+        Dataset<Row> df = Utils.createEmptyRDD(spark, schemaStructured);
 
-
-        Dataset<Row> df= Utils.createEmptyRDD(spark,schemaStructured);
-
-        Utils.createHiveTable(df,config.topic,spark);
+        Utils.createHiveTable(df, config.topic, spark);
 
         Map<String, Object> kafkaParams = new HashMap<>();
 
         kafkaParams.put("bootstrap.servers", config.bootstrap_servers);
         kafkaParams.put("key.deserializer", StringDeserializer.class);
-//        kafkaParams.put("value.deserializer", ByteArrayDeserializer.class);
         kafkaParams.put("value.deserializer", io.confluent.kafka.serializers.KafkaAvroDeserializer.class);
-        kafkaParams.put("schema.registry.url","http://10.1.1.90:8081");
+        kafkaParams.put("schema.registry.url", "http://10.1.1.90:8081");
         kafkaParams.put("group.id", "groupid");
         kafkaParams.put("auto.offset.reset", "latest");
-        kafkaParams.put("enable.auto.commit", false);
+        kafkaParams.put("enable.auto.commit", true);
 
         Collection<String> topics = Arrays.asList(config.topic);
-        /**
-
-        JavaInputDStream<ConsumerRecord<String, byte[]>> stream =
-                KafkaUtils.createDirectStream(
-                        ssc,
-                        LocationStrategies.PreferConsistent(),
-                        ConsumerStrategies.<String, byte[]>Subscribe(topics, kafkaParams)
-                );
-
-        stream.map(message -> recordInjection.invert(message.value()).get()).
-                foreachRDD( javaRDD -> {
-                    JavaRDD<Row> rddOfRows =javaRDD.map(fields -> RowFactory.create(fields));
-                    Dataset<Row> df2 =  spark.createDataFrame(rddOfRows,schemaStructured);
-                    df2.write().insertInto(config.topic);
-
-        });
-*/
 
 
         JavaInputDStream<ConsumerRecord<String, GenericRecord>> stream =
@@ -133,36 +105,16 @@ private static StructType schemaStructured = null;
                 );
 
         stream.map(message -> message.value()).
-                foreachRDD( javaRDD -> {
-//                    System.out.println(" count="+javaRDD.count()+" antal partitioner="+javaRDD.getNumPartitions()+"");
+                foreachRDD(javaRDD -> {
+                    JavaRDD<Row> newRDD = javaRDD.map(x -> {
+                        return Utils.avroToRowConverter(x, schemaStructured);
+                    });
 
-                                JavaRDD<Row> newRDD= javaRDD.map(x->{
-                                    return avroToRowConverter(x,schemaStructured);
-                                });
-
-                    System.out.println(" ROW count="+newRDD.count()+" antal ROW partitioner="+newRDD.getNumPartitions()+"");
-                    Dataset<Row> df2 =  spark.createDataFrame(newRDD,schemaStructured);
-                    df2.printSchema();
-                    df2.show(10);
+                    System.out.println(" ROW count=" + newRDD.count() + " antal ROW partitioner=" + newRDD.getNumPartitions() + "");
+                    Dataset<Row> df2 = spark.createDataFrame(newRDD, schemaStructured);
                     df2.write().insertInto(config.topic);
-
-
-                    /**
-        JavaRDD<Row> rddOfRows =javaRDD.map(fields -> RowFactory.create(fields));
-        Dataset<Row> df2 =  spark.createDataFrame(rddOfRows,schemaStructured);
-        df2.printSchema();
-        df.show(10);
-        df2.write().insertInto(config.topic);
-                     */
-
                 });
 
-
-/**
-        JavaDStream<byte[]> a=stream.map(message -> message.value() );
-        a.
-        a .flatMap(iiii-> {
-        });
 
 /**
  * Update offsets if sucess storing all external
@@ -170,22 +122,20 @@ private static StructType schemaStructured = null;
  *
  * */
 
-        stream.foreachRDD( rdd -> {
+/**
+ *  Temporary disable offset handling
 
-            OffsetRange[] offsetRanges = ((HasOffsetRanges) rdd.rdd()).offsetRanges();
-            if(null== TaskContext.get()) {
-                return;
-            }
-            OffsetRange o = offsetRanges[TaskContext.get().partitionId()];
-            System.out.println(o.topic() + " " + o.partition() + " " + o.fromOffset() + " " + o.untilOffset());
-
-            ((CanCommitOffsets) stream.inputDStream()).commitAsync(offsetRanges);
-
-            });
-
-
-
-            ssc.start();
+ stream.foreachRDD( rdd -> {
+ OffsetRange[] offsetRanges = ((HasOffsetRanges) rdd.rdd()).offsetRanges();
+ if(null== TaskContext.get()) {
+ return;
+ }
+ OffsetRange o = offsetRanges[TaskContext.get().partitionId()];
+ System.out.println(o.topic() + " " + o.partition() + " " + o.fromOffset() + " " + o.untilOffset());
+ ((CanCommitOffsets) stream.inputDStream()).commitAsync(offsetRanges);
+ });
+ **/
+        ssc.start();
         try {
             ssc.awaitTermination();
         } catch (InterruptedException e) {
@@ -195,22 +145,4 @@ private static StructType schemaStructured = null;
 
     }
 
-// Got this from Ram Ghadiyaram
-    private static Row avroToRowConverter(GenericRecord avroRecord,StructType structType) {
-        if (null == avroRecord) {
-            return null;
-        }
-
-        Object[] objectArray = new Object[avroRecord.getSchema().getFields().size()];
-//        StructType structType = (StructType) SchemaConverters.toSqlType(avroRecord.getSchema()).dataType();
-        for (Schema.Field field : avroRecord.getSchema().getFields()) {
-            if(field.schema().getType().toString().equalsIgnoreCase("STRING") || field.schema().getType().toString().equalsIgnoreCase("ENUM")){
-                objectArray[field.pos()] = ""+avroRecord.get(field.pos());
-            }else {
-                objectArray[field.pos()] = avroRecord.get(field.pos());
-            }
-        }
-
-        return new GenericRowWithSchema(objectArray, structType);
-    }
 }
