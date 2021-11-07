0aa3f0fb1acaebb5b052ecfa1c9eacd2946a597b
Rickard Lundin <rickard@x14.se>
2021-11-07 19:40:05 +0100

    print dframe

--- spark232job-mod/src/main/java/dk/ignalina/lab/spark232/RddSparkStreamingKafka.java
+++ spark232job-mod/src/main/java/dk/ignalina/lab/spark232/RddSparkStreamingKafka.java
@@ -139,6 +139,7 @@ private static StructType schemaStructured = null;
                     JavaRDD<Row> rddOfRows =javaRDD.map(fields -> RowFactory.create(fields));
                     Dataset<Row> df2 =  spark.createDataFrame(rddOfRows,schemaStructured);
                     df2.printSchema();
+                    df.show(10);
 //                    df2.write().insertInto(config.topic);
 
                 });
