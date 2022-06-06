197380747b0cc825c4349e7ed7d5c352bb031659
Rickard Ernst Björn Lundin <33436048+Ignalina@users.noreply.github.com>
2022-06-06 19:57:23 +0200

    Update submit_eventdriven301.sh

--- infra/submit_eventdriven301.sh
+++ infra/submit_eventdriven301.sh
@@ -4,10 +4,10 @@ rm -rf /home/spark/.ivy2/cache
 spark-submit \
 --conf "spark.jars.ivySettings=/home/spark/.ivy2/ivysettings.xml" \
 --class dk.ignalina.lab.spark301.KafkaEventDrivenSparkJob \
---repositories https://nexus.x14.se/repository/maven-public,https://nexus.x14.se/repository/maven-snapshots \
---packages dk.ignalina.lab.spark301:welder-spark-job:0.0.1,org.apache.spark:spark-streaming-kafka-0-10_2.12:3.0.1,org.apache.spark:spark-streaming_2.12:3.0.1 \
+--repositories https://nexus.x14.se/repository/maven-public,https://nexus.x14.se/repository/maven-releases \
+--packages dk.ignalina.lab.spark301:welder-spark-job:0.0.3,org.apache.spark:spark-streaming-kafka-0-10_2.12:3.0.1,org.apache.spark:spark-streaming_2.12:3.0.1 \
 --driver-memory 4g \
---master spark://10.1.1.190:6067 \
+--master spark://10.1.1.196:7077 \
 --driver-cores 4 \
 --verbose \
 --deploy-mode cluster \
