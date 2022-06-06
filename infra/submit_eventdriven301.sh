66e64855f62720787c16340b9df7c52a6b71eacc
Rickard Ernst Björn Lundin <33436048+Ignalina@users.noreply.github.com>
2022-06-06 23:58:58 +0200

    worked

--- infra/submit_eventdriven301.sh
+++ infra/submit_eventdriven301.sh
@@ -5,7 +5,7 @@ spark-submit \
 --conf "spark.jars.ivySettings=/home/spark/.ivy2/ivysettings.xml" \
 --class dk.ignalina.lab.spark301.KafkaEventDrivenSparkJob \
 --repositories https://nexus.x14.se/repository/maven-public,https://nexus.x14.se/repository/maven-releases \
---packages dk.ignalina.lab.spark301:welder-spark-job:0.0.3,org.apache.spark:spark-streaming-kafka-0-10_2.12:3.0.3,org.apache.spark:spark-streaming_2.12:3.0.3 \
+--packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.0.3,dk.ignalina.lab.spark301:welder-spark-job:0.0.3 \
 --driver-memory 4g \
 --master spark://10.1.1.196:7077 \
 --driver-cores 4 \
@@ -13,3 +13,6 @@ spark-submit \
 --deploy-mode cluster \
 ksmb.jar \
 testar kafka 10.1.1.90:9092 10.1.1.90:8081 testar-value /tmp/spark/checkpoint latest PERMISSIVE 1000 groupid &
+
+
+#--packages dk.ignalina.lab.spark301:welder-spark-job:0.0.3,org.apache.spark:spark-streaming-kafka-0-10_2.12:3.0.3,org.apache.spark:spark-streaming_2.12:3.0.3 \
