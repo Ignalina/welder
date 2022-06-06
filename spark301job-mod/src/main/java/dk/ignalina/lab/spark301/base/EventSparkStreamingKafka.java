f47b596e2407d249f1e751b16c91a2a58412274e
Rickard Lundin <rickard@ignalina.dk>
2022-06-06 12:13:27 +0200

    added spark301

---
+++ spark301job-mod/src/main/java/dk/ignalina/lab/spark301/base/EventSparkStreamingKafka.java
@@ -0,0 +1,81 @@
+/*
+ * MIT No Attribution
+ *
+ * Copyright 2021 Rickard Lundin (rickard@ignalina.dk)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package dk.ignalina.lab.spark301.base;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.api.java.JavaInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka010.ConsumerStrategies;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
+
+import java.util.*;
+
+public class EventSparkStreamingKafka {
+
+    public static Action actionImpl;
+
+    public static SparkConf CreateSparkConf(String appName) {
+        SparkConf conf = new SparkConf().setAppName(appName);
+        conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerialize");
+        conf.registerKryoClasses((Class<ConsumerRecord>[]) Arrays.asList(ConsumerRecord.class).toArray());
+        return conf;
+    }
+
+    public static void callForEachRdd(JavaRDD<ConsumerRecord<String, GenericRecord>> rdd) {
+
+        if (null == actionImpl) {
+            return; //
+        }
+
+        JavaRDD<ConsumerRecord<String, GenericRecord>> rdd1 = rdd;
+        System.out.println("rdd=" + rdd1.toDebugString());
+        List<ConsumerRecord<String, GenericRecord>> rows = rdd1.collect();
+
+        JsonParser parser = new JsonParser();
+
+        for (ConsumerRecord<String, GenericRecord> cr : rows) {
+            System.out.println("Parse string to json object" + cr.value());
+            String message = ""+cr.value();
+            JsonObject jo = null;
+            try {
+                jo = parser.parse(message).getAsJsonObject();
+            } catch (IllegalStateException ei) {
+                System.out.println("Invalid unparsable json:" + ei.toString());
+            }
+
+            if (jo != null) {
+                actionImpl.fire(jo);
+            }
+
+        }
+    }
+
+
+}
---
+++ spark301job-mod/src/main/java/dk/ignalina/lab/spark301/base/Utils.java
@@ -0,0 +1,108 @@
+/*
+ * MIT No Attribution
+ *
+ * Copyright 2021 Rickard Lundin (rickard@ignalina.dk)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package dk.ignalina.lab.spark301.base;
+
+//import com.databricks.spark.avro.SchemaConverters;
+import io.confluent.kafka.schemaregistry.client.rest.RestService;
+import io.confluent.kafka.schemaregistry.client.rest.entities.Schema;
+import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema;
+import org.apache.spark.sql.types.StructType;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Utils {
+
+    public static class Config {
+        public String topic = "topic";
+        public String format;
+        public String bootstrap_servers = "10.1.1.90:9092";
+        public String schemaRegistryURL = "10.1.1.90:8081";
+        public String subjectValueName;
+        public String startingOffsets;
+        public String checkpointDir;
+        public String mode = "PERMISSIVE";
+        public String failOnDataLoss = "false";
+        public String master = "local[2]";
+        public int msIntervall = 2000;
+        public Object groupId="groupid";
+        public Object autoOffsetReset="latest";
+
+        public  Config(String[] args) {
+            topic = args[0];
+            format = args[1];
+            bootstrap_servers = args[2];
+            schemaRegistryURL = args[3];
+            subjectValueName = args[4];
+            checkpointDir = args[5];
+            startingOffsets = args[6];
+            mode = args[7];
+            msIntervall = Integer.parseInt(args[8]); ;
+            groupId=args[9];
+        }
+
+    }
+
+    static public String getLastestSchema(Config config) throws IOException, RestClientException {
+        return getLastestSchema(config.schemaRegistryURL, config.subjectValueName);
+    }
+
+    static public String getLastestSchema(String schemaRegistryURL, String subjectValueName) throws IOException, RestClientException {
+        RestService rs = new RestService(schemaRegistryURL);
+        Schema valueRestResponseSchema = rs.getLatestVersion(subjectValueName);
+        return valueRestResponseSchema.getSchema().toString();
+    }
+
+
+    static public void createHiveTable(Dataset<Row> df, String tableName, SparkSession spark) {
+        spark.sql("use hiveorg_prod");
+
+
+        String tables[] = spark.sqlContext().tableNames();
+        if (Arrays.asList(tables).contains(tables)) {
+            return;
+        }
+
+        String tmpTableName = "my_temp" + tableName;
+
+        df.createOrReplaceTempView(tmpTableName);
+        spark.sql("drop table if exists " + tableName);
+        spark.sql("create table " + tableName + " as select * from " + tmpTableName);
+
+    }
+
+    static public Dataset<Row> createEmptyRDD(SparkSession spark, StructType schema) {
+        return spark.createDataFrame(new ArrayList<>(), schema);
+    }
+
+
+
+
+}
