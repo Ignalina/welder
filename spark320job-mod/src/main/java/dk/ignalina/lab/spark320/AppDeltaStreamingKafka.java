9b6475d3b7caf1965727b358f2e68d1bfa13e472
Rickard <rickard@ignalina.dk>
2022-06-20 21:13:00 +0200

    320 ongoing

--- spark320job-mod/src/main/java/dk/ignalina/lab/spark320/AppDeltaStreamingKafka.java
+++ spark320job-mod/src/main/java/dk/ignalina/lab/spark320/AppDeltaStreamingKafka.java
@@ -23,31 +23,47 @@ import org.apache.spark.*;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.streaming.Duration;
 import org.apache.spark.streaming.api.java.*;
+import dk.ignalina.lab.spark320.base.Utils;
+
 
 
 
 public class AppDeltaStreamingKafka {
 
 
+    private static String authType;
+
     public static void main(String[] args) {
 
+        Utils.Config config = new Utils.Config(args);
+//        JavaStreamingContext ssc = new JavaStreamingContext(new SparkConf().setAppName("v20220612 spark 3.0.1 Streaming /event driven spark job"), new Duration(config.msIntervall));
+//        SparkSession spark=Utils.createS3SparkSession(config);
+//        JavaInputDStream<ConsumerRecord<String, GenericRecord>> stream = Utils.createStream(config,ssc);
+        Utils.decorateS3SparkSession(config);
+        //for a local spark instance
         SparkConf conf = new SparkConf()
                 .setAppName("appName")
                 .setMaster("spark://10.1.1.193:7077")
                 .set("spark.submit.deployMode", "cluster")
-                .set("spark.jars.packages", "org.projectnessie:nessie-deltalake:0.16.0")
-                .set("spark.hadoop.nessie.url", "10.1.1.192:19120")
-                .set("spark.hadoop.nessie.ref", "branch")
-                .set("spark.hadoop.nessie.authentication.type","NONE")
+                .set("spark.jars.packages", "org.projectnessie:nessie-deltalake:0.30.0,org.projectnessie:nessie-spark-3.2-extensions:0.30.0")
+                .set("spark.hadoop.nessie.url", config.url)
+                .set("spark.hadoop.nessie.ref", config.branch)
+                .set("spark.hadoop.nessie.authentication.type", config.authType)
                 .set("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
-                .set("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+                .set("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension,org.projectnessie.spark.extensions.NessieSpark32SessionExtensions")
                 .set("spark.delta.logStore.class", "org.projectnessie.deltalake.NessieLogStore")
                 .set("spark.delta.logFileHandler.class", "org.projectnessie.deltalake.NessieLogFileMetaParser");
 
+
         JavaStreamingContext ssc = new JavaStreamingContext(conf, new Duration(1000));
 
-        SparkSession spark = SparkSession.builder()
+//        spark = SparkSession.builder()
 //                .master("local[2]")
+//                .config(conf)
+//                .getOrCreate();
+
+        SparkSession spark = SparkSession.builder()
+                .master(config.master)
                 .config(conf)
                 .getOrCreate();
     }
---
+++ spark320job-mod/src/main/java/dk/ignalina/lab/spark320/base/Utils.java
@@ -0,0 +1,146 @@
+/*
+ * MIT No Attribution
+ *
+ * Copyright 2021 Rickard Lundin (rickard@ignalina.dk)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package dk.ignalina.lab.spark320.base;
+
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.StructType;
+import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.api.java.JavaInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka010.ConsumerStrategies;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
+
+import java.util.*;
+
+
+public class Utils {
+
+    public static class Config {
+        public String topic = "topic";
+        public String format;
+        public String bootstrap_servers = "10.1.1.90:9092";
+        public String schemaRegistryURL = "10.1.1.90:8081";
+        public String subjectValueName;
+        public String startingOffsets;
+        public String checkpointDir;
+        public String mode = "PERMISSIVE";
+        public String failOnDataLoss = "false";
+        public String master = "local[2]";
+        public int msIntervall = 2000;
+        public Object groupId="groupid";
+        public Object autoOffsetReset="latest";
+        public String s3AccessKey;
+        public String s3SecretKey;
+        public String s3EndPoint;
+        public String branch;
+        public String url;
+        public String authType;
+
+        public  Config(String[] args) {
+            topic = args[0];
+            format = args[1];
+            bootstrap_servers = args[2];
+            schemaRegistryURL = args[3];
+            subjectValueName = args[4];
+            checkpointDir = args[5];
+            startingOffsets = args[6];
+            mode = args[7];
+            msIntervall = Integer.parseInt(args[8]); ;
+            groupId=args[9];
+            s3AccessKey=args[10];
+            s3SecretKey=args[11];
+            s3EndPoint=args[12];
+            branch=args[13];
+            url=args[14];
+            authType=args[15];
+        }
+
+    }
+
+
+// TODO convert from hive to Delta ASAP
+    static public void createDeltaTable(Dataset<Row> df, String tableName, SparkSession spark) {
+//        spark.sql("use hiveorg_prod");
+
+
+//        String tables[] = spark.sqlContext().tableNames();
+//        if (Arrays.asList(tables).contains(tables)) {
+//            return;
+//        }
+
+//        String tmpTableName = "my_temp" + tableName;
+
+//        df.createOrReplaceTempView(tmpTableName);
+//        spark.sql("drop table if exists " + tableName);
+//        spark.sql("create table " + tableName + " as select * from " + tmpTableName);
+
+    }
+
+    static public Dataset<Row> createEmptyRDD(SparkSession spark, StructType schema) {
+        return spark.createDataFrame(new ArrayList<>(), schema);
+    }
+    public static SparkConf CreateSparkConf(String appName) {
+        SparkConf conf = new SparkConf().setAppName(appName);
+        return conf;
+    }
+
+    static public SparkSession decorateS3SparkSession(Utils.Config config) {
+        SparkSession spark = SparkSession.builder().master(config.master).
+                config("spark.hadoop.fs.s3a.impl","org.apache.hadoop.fs.s3a.S3AFileSystem").
+                config("fs.s3a.access.key",config.s3AccessKey).
+                config("fs.s3a.secret.key",config.s3SecretKey).
+                config("fs.s3a.endpoint", config.s3EndPoint).
+                config("fs.s3a.impl","org.apache.hadoop.fs.s3a.S3AFileSystem").
+                config("fs.s3a.path.style.access","true").
+                getOrCreate();
+        return spark;
+    }
+    static public JavaInputDStream<ConsumerRecord<String, GenericRecord>> createStream(Utils.Config config,JavaStreamingContext ssc) {
+
+        Map<String, Object> kafkaParams = new HashMap<>();
+
+        kafkaParams.put("bootstrap.servers", config.bootstrap_servers);
+        kafkaParams.put("key.deserializer", StringDeserializer.class);
+        kafkaParams.put("value.deserializer", StringDeserializer.class);
+        kafkaParams.put("group.id", config.groupId);
+        kafkaParams.put("auto.offset.reset", config.startingOffsets);
+        kafkaParams.put("enable.auto.commit", true);
+
+        Collection<String> topics = Arrays.asList(config.topic);
+
+        JavaInputDStream<ConsumerRecord<String, GenericRecord>> stream =
+                KafkaUtils.createDirectStream(
+                        ssc,
+                        LocationStrategies.PreferConsistent(),
+                        ConsumerStrategies.<String, GenericRecord>Subscribe(topics, kafkaParams)
+                );
+
+        return stream;
+    }
+
+
+}
