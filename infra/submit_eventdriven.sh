2d71d52c005263ecd9b6c307815f2b9794af7257
Rickard Ernst Björn <rickard@ignalina.dk>
2023-04-23 15:39:26 +0200

    spark332

--- infra/submit_eventdriven.sh
+++ infra/submit_eventdriven.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 rm -rf /home/spark/.ivy2/cache
+export SPARK_HOME=/opt/spark-2.3.2-bin-hadoop2.7
 
-spark-submit \
+$SPARK_HOME/bin/spark-submit \
 --conf "spark.jars.ivySettings=/home/spark/.ivy2/ivysettings.xml" \
 --class dk.ignalina.lab.spark232.EventSparkStreamingKafka \
---repositories https://nexus.x14.se/repository/maven-public,https://nexus.x14.se/repository/maven-snapshots \
+--repositories https://nexus.x14.se/repository/maven-public,https://nexus.x14.se/repository/maven-relase \
 --packages org.apache.avro:avro:1.9.2,com.databricks:spark-avro_2.11:4.0.0,dk.ignalina.lab.spark232:welder-spark-job:0.0.2,org.apache.spark:spark-streaming-kafka-0-10_2.11:2.3.2,org.apache.spark:spark-streaming_2.11:2.3.2 \
 --driver-memory 4g \
 --master spark://10.1.1.190:6067 \
 --driver-cores 4 \
 --verbose \
 --deploy-mode cluster \
-ksmb.jar \
+/home/spark/ksmb.jar \
 testar kafka 10.1.1.90:9092 10.1.1.90:8081 testar-value /tmp/spark/checkpoint latest PERMISSIVE &
---
+++ infra/submit_eventdriven332.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+rm -rf /home/spark/.ivy2/cache
+export SPARK_HOME=/opt/spark-3.3.2-bin-hadoop3
+export VER=0.0.55
+wget https://nexus.x14.se/repository/maven-releases/dk/ignalina/lab/spark301/welder-spark-job/${VER}/welder-spark-job-${VER}.jar
+
+${SPARK_HOME}/bin/spark-submit \
+--conf "spark.jars.ivySettings=/home/spark/.ivy2/ivysettings.xml" \
+--class dk.ignalina.lab.spark301.KafkaEventDrivenSparkJob \
+--repositories https://nexus.x14.se/repository/maven-public,https://nexus.x14.se/repository/maven-releases \
+--packages org.apache.hadoop:hadoop-aws:3.0.1,com.amazonaws:aws-java-sdk-bundle:1.12.236 \
+--driver-memory 4g \
+--master spark://10.1.1.93:7077 \
+--driver-cores 4 \
+--verbose \
+--deploy-mode cluster \
+--supervise \
+welder-spark-job-${VER}.jar \
+testar kafka 10.1.1.90:9092 10.1.1.90:8081 testar-value /tmp/spark/checkpoint latest PERMISSIVE 1000 groupid rickard tihi apiminio.x14.se &
+
+#--packages dk.ignalina.lab.spark301:welder-spark-job:0.0.15,org.apache.spark:spark-streaming-kafka-0-10_2.12:3.0.1,org.apache.spark:spark-streaming_2.12:3.0.1 \
+
+#--packages dk.ignalina.lab.spark301:welder-spark-job:0.0.3,org.apache.spark:spark-streaming-kafka-0-10_2.12:3.0.3,org.apache.spark:spark-streaming_2.12:3.0.3 \
